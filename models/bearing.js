1eefaf91fa024eab5cdb36b45a3f6b3f080f7e36
Danny <you@example.com>
2023-01-03 23:13:29 -0500

    wip: bearing schema update

--- models/bearing.js
+++ models/bearing.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
 
 const bearingSchema = new mongoose.Schema({
-    name: {type: String},
-    img: {type: String},
-    price: {type: Number}
-})
-
-const Bearing = mongoose.model('Bearing', bearingSchema);
+    name: String,
+    img: String,
+    price: Number,
+  });
+  
+  const Bearing = mongoose.models.Bearing || mongoose.model('Bearing', bearingSchema);
 
 module.exports = Bearing;
